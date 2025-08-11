f88fd8abc585212ffcfe3e18f9fc8f586ddc6f01
bakkiasri <bakkiasri2001@gmail.com>
2025-08-11 17:10:46 +0530

    home page

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../layout/navbar";
-import logoImg from "../assets/images.jpg"; // Optional logo
+import logoImg from "../assets/flower_page.jpg"; 
 import About from "./about";
 import { useNavigate } from "react-router-dom";
+import Stock from "../assets/stock_images.jpg"
 function Home() {
   
   return (
@@ -39,24 +40,18 @@ function Home() {
       </div>
 
       {/* Categories Section */}
-      <div className="bg-white py-12">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-10">
-          About
-        </h2>
-        <div className="grid gap-8 px-10 md:grid-cols-3">
-          <div className="bg-gray-100 p-6 rounded-lg shadow hover:shadow-lg transition">
-            <h3 className="text-xl font-semibold mb-2 text-gray-800">Row 1</h3>
-            <p className="text-gray-600">Category description goes here.</p>
-          </div>
-          <div className="bg-gray-100 p-6 rounded-lg shadow hover:shadow-lg transition">
-            <h3 className="text-xl font-semibold mb-2 text-gray-800">Row 2</h3>
-            <p className="text-gray-600">Category description goes here.</p>
-          </div>
-          <div className="bg-gray-100 p-6 rounded-lg shadow hover:shadow-lg transition">
-            <h3 className="text-xl font-semibold mb-2 text-gray-800">Row 3</h3>
-            <p className="text-gray-600">Category description goes here.</p>
-          </div>
+      <div className='pt-15 pb-7 px-3 md:px-15 bg-[#F7F8FB]'>
+         <div className="grid  items-center  p-7 px-2 md:px-15 grid-cols-1 md:grid-cols-2  ">
+      <div className='text-4xl   font-medium text-[#100B39] ps-10 pt-4 '>
+        Our Stock
+      </div>
+      <div className=' p-5 pe-7 text-lg/6 text-justify  space-y-4'>
+        <img src={Stock}   className=" object-contain p-3 rounded-lg transition-transform duration-300 ease-in-out hover:scale-110"
+/>
+         
         </div>
+        </div>
+       
       </div>
     </div>
   );
